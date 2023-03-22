a8cd0a1bef0d0946584040247df785a368743567
wtdavis <wtdavis@wesleyan.edu>
2023-03-22 12:16:16 -0400

    lunchtime

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -2,17 +2,29 @@ import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
 
+import {useTheme} from "../../context/ThemeContext.js"
+
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
+  
+  const {themeName} = useTheme()
+  let theme = themeName === "day" ? (<img  className='greenhouse-img' src={dayImage}
+  alt='greenhouse'
+/>) : (<img  className='greenhouse-img'
+            src={nightImage}
+            alt='greenhouse' 
+      />)
 
   return (
     <section>
-      <img  className='greenhouse-img'
+      {theme}
+      {/* {<img  className='greenhouse-img'
             src={dayImage}
             alt='greenhouse' 
-      />
+      />} */}
+
       <LightSwitch />
       <ClimateStats />
     </section>
--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,22 @@
 import './LightSwitch.css';
+import {useTheme} from "../../context/ThemeContext.js"
+
 
 function LightSwitch() {
+  const {themeName, setThemeName} = useTheme()
+  console.log(themeName, setThemeName)
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    // console.log(e.target.id)
+    setThemeName(e.target.id)
+    alert (`changing themes to ${e.target.id}`)
+  }
+
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" id="day" onClick={handleClick}>DAY</div>
+      <div className="off" id="night" onClick={handleClick}>NIGHT</div>
     </div>
   );
 }
